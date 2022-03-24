9414de91445008566cd84cd147a71a3aa523ba46
Leszek Wydro <leszek.wydro@gmail.com>
2022-03-24 17:36:44 +0100

    add books filter

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,3 @@
-
 {
   'use strict';
 
@@ -15,6 +14,7 @@
       bookName: '.book_name',
       price: '.product__base-price',
       bookImg: '.book__image',
+      form: '.filters',
     },
 
   };
@@ -22,6 +22,7 @@
   const template = {
     book: Handlebars.compile(document.querySelector(select.templateOf.books).innerHTML), 
   };
+  const filters = [];
 
 
   const render = function(){
@@ -38,10 +39,9 @@
     }
   };
 
-  const favoriteBooks = [];
-
   const initActions = function(){
 
+    const favoriteBooks = [];
     const container = document.querySelector(select.list.bookList);
     
     container.addEventListener('dblclick', function(event){
@@ -51,7 +51,7 @@
 
       console.log(elementClicked);
 
-      if(elementClicked.classList.contains('.book__image')){
+      if(elementClicked.classList.contains('book__image')){
 
         const idBook = elementClicked.getAttribute('data-id');
         
@@ -70,8 +70,51 @@
         }
       }
     });
-  };
 
+    const filters = [];
+    const bookFilter = document.querySelector(select.all.form);
+
+    bookFilter.addEventListener('click', function(event){
+
+      const clickedElement = event.target;
+
+      if (clickedElement.tagName == 'INPUT' && clickedElement.type == 'checkbox' && clickedElement.name == 'filter'){
+
+        if (clickedElement.checked) {
+          filters.push(clickedElement.value);
+        }else{
+          const filterIndex = filters.indexOf(clickedElement.value);
+          filters.splice(filterIndex, 1);
+        }
+      }
+      console.log('clickedElement', clickedElement);
+      filterBooks ();
+    });
+  };
+    
+  
+  const filterBooks = function(){
+    
+
+    for (let book of dataSource.books){
+
+      const filterBook = document.querySelector('.book__image[data-id="' + book.id + '"]');
+
+      let shouldBeHidden = false;
+      for(const filter of filters){
+        if(!book.details[filter]){
+          shouldBeHidden = true;
+          break;
+        }
+      }
+      if(shouldBeHidden === true){
+        filterBook.classList.add('hidden');
+      }else{
+        filterBook.classList.remove('hidden');
+      }
+    }
+  };
   render();
   initActions();
 }
+
